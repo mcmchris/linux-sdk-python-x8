6fd1158f791b132fd56cbc3b673aea80bc185c1e
Christopher Méndez <mcmchris@hotmail.com>
2024-04-10 11:39:14 -0400

    Color switch test

--- examples/image/classify.py
+++ examples/image/classify.py
@@ -116,12 +116,12 @@ def main(argv):
                     print('', flush=True)
                
                 ret, buffer = cv2.imencode('.jpg', img)
-                #buffer = cv2.cvtColor(ret, cv2.COLOR_BGR2GRAY)
+                buffer = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
                 frame = buffer.tobytes()
                 yield (b'--frame\r\n'
                     b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
 
-                next_frame = now() + 30
+                next_frame = now() + 100
         finally:
             if (runner):
                 runner.stop()
