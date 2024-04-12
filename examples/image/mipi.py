10bfe6114fcb39987849436df7df0da11b942414
Christopher Méndez <mcmchris@hotmail.com>
2024-04-12 14:33:34 -0400

    examples added

--- examples/image/mipi.py
+++ examples/image/mipi.py
@@ -1,7 +1,7 @@
 import cv2
 
 #cap = cv2.VideoCapture(0,cv2.CAP_V4L)
-cap = cv2.VideoCapture("v4l2src device=/dev/video0 ! video/x-raw,format=RGB,width=1280,height=720,framerate=30/1 ! videoconvert ! video/x-raw,format=BGR ! appsink drop=1", cv2.CAP_GSTREAMER)
+cap = cv2.VideoCapture("v4l2src device=/dev/video0 ! video/x-raw,format=BG10,width=640,height=480,framerate=30/1 ! videoconvert ! video/x-raw,format=BGR ! appsink drop=1", cv2.CAP_GSTREAMER)
 
 
 print (cap.isOpened())
