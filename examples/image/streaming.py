b87d51fae180daaac4d42655ce2719d4a0473f59
Christopher Méndez <mcmchris@hotmail.com>
2024-04-12 09:46:34 -0400

    streaming example added

--- examples/image/streaming.py
+++ examples/image/streaming.py
@@ -5,7 +5,7 @@ import cv2
 
 app = Flask(__name__, static_folder='templates/assets')
 
-camera = cv2.VideoCapture(1, cv2.CAP_DSHOW)
+camera = cv2.VideoCapture(1)
 
 def generate():   
     while True:
