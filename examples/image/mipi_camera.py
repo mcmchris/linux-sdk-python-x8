81c2b5adf4d3a78ecc1d11fb4789760cd36a1880
Christopher Méndez <mcmchris@hotmail.com>
2024-04-12 10:38:33 -0400

    mipi test

--- examples/image/mipi_camera.py
+++ examples/image/mipi_camera.py
@@ -1,6 +1,6 @@
 from v4l2py import Device
 
-with Device.from_id(0) as cam:
+with Device.from_id(1) as cam:
     for i, frame in enumerate(cam):
        print(f"frame #{i}: {len(frame)} bytes")
        if i > 9:
