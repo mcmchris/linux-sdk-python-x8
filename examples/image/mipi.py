93d025bf77af212d27a8361cfdf3bfb9a85dc1ae
Christopher Méndez <mcmchris@hotmail.com>
2024-04-12 15:25:34 -0400

    mipi test

--- examples/image/mipi.py
+++ examples/image/mipi.py
@@ -7,7 +7,7 @@ app = Flask(__name__, static_folder='templates/assets')
 
 def main(argv):
 
-    cap = cv2.VideoCapture('v4l2src -d /dev/video0',cv2.CAP_V4L2)
+    cap = cv2.VideoCapture('/dev/video0',cv2.CAP_V4L2)
 
     if not cap.isOpened():
         print('Failed to open camera');
