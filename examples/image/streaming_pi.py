b0dbaad64a7eaec52c74f0faf69eb6e76c2535cc
Christopher Méndez <mcmchris@hotmail.com>
2024-04-21 10:11:56 -0400

    pi test

--- examples/image/streaming_pi.py
+++ examples/image/streaming_pi.py
@@ -51,9 +51,6 @@ def main(argv):
 
     camera = cv2.VideoCapture(videoCaptureDeviceId)
 
-    face_detector = cv2.CascadeClassifier(cv2.data.haarcascades +
-     "haarcascade_frontalface_default.xml")
-
     ret = camera.read()[0]
     if ret:
         backendName = camera.getBackendName()
@@ -71,10 +68,6 @@ def main(argv):
         ret, img = camera.read()
         if ret:
             gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-            faces = face_detector.detectMultiScale(gray, 1.3, 5)
-            for (x, y, w, h) in faces:
-                cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0),2)
-
             #img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
             (ret, buffer) = cv2.imencode('.jpg', img)
             if not ret:
