f6719fa83e06b03322baebbdcdf9f748ee7bfe0c
Christopher Méndez <mcmchris@hotmail.com>
2024-04-21 10:39:21 -0400

    pi test

--- examples/image/mipi_pi.py
+++ examples/image/mipi_pi.py
@@ -1,3 +1,18 @@
-from picamera2 import Picamera2
+import time
+
+from picamera2 import Picamera2, Preview
+
 picam2 = Picamera2()
-picam2.start_and_capture_file("test1.jpg")
+
+preview_config = picam2.create_preview_configuration(main={"size": (800, 600)})
+picam2.configure(preview_config)
+
+picam2.start_preview(Preview.QTGL)
+
+picam2.start()
+time.sleep(2)
+
+metadata = picam2.capture_file("test.jpg")
+print(metadata)
+
+picam2.close()
