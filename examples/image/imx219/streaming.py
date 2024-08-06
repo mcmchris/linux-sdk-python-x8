02d57463fbde9de6d29eeef95eff9034b61593a9
Christopher Méndez <49886387+mcmchris@users.noreply.github.com>
2024-08-06 19:38:02 -0400

    streaming.py edited

--- examples/image/imx219/streaming.py
+++ examples/image/imx219/streaming.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import device_patches       # Device specific patches for Jetson Nano (needs to be before importing cv2)
+#import device_patches       # Device specific patches for Jetson Nano (needs to be before importing cv2)
 
 import cv2
 import time
